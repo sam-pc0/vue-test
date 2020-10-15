2dd187321189173a760fad203cd4c52f7bcc394d
sam <sscambara@gmail.com>
2020-10-15 09:56:44 -0600

    feat: custom configs to root project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Buefy from 'buefy';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Buefy,{
+    defaultIconPack: 'fas',
+    defaultContainerElement: '#app',
+} );
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
